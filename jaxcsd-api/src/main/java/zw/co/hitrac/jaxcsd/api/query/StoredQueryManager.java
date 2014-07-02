9381639cd02435b78b2272d7c28111699eb587da
Charles Chigoriwa <cchigoriwa@gmail.com>
2014-07-02 10:22:54 +0200

    Added CsdQueryClient code in StoredQueryManager

--- jaxcsd-api/src/main/java/zw/co/hitrac/jaxcsd/api/query/StoredQueryManager.java
+++ jaxcsd-api/src/main/java/zw/co/hitrac/jaxcsd/api/query/StoredQueryManager.java
@@ -1,6 +1,7 @@
 package zw.co.hitrac.jaxcsd.api.query;
 
 import java.util.List;
+import zw.co.hitrac.jaxcsd.api.domain.CSD;
 import zw.co.hitrac.jaxcsd.api.domain.Facility;
 import zw.co.hitrac.jaxcsd.api.domain.OtherID;
 import zw.co.hitrac.jaxcsd.api.domain.Provider;
@@ -11,73 +12,51 @@ import zw.co.hitrac.jaxcsd.api.util.JaxCsdUtil;
  * @author Charles Chigoriwa
  */
 public class StoredQueryManager {
-    
+
     public static List<Provider> getProviders(RequestParams requestParams, String httpAddress) {
-        CareServicesRequest careServicesRequest = new CareServicesRequest(new Function(StoredQueryConstants.PROVIDER_SEARCH, requestParams));
-        String xmlRequestBody = careServicesRequest.marshal();
-        System.out.println("xmlBody=" + xmlRequestBody);
-        String xmlResponseBody = JaxCsdUtil.executeXmlPost(xmlRequestBody, httpAddress);
-        //System.out.print("response=" + xmlResponseBody);
+        CsdQueryClient csdQueryClient = new CsdQueryClient();
         try {
-            /*CSD csd = CSDHandler.getCSD(xmlResponseBody);
-            if(csd!=null && csd.getProviderDirectory()!=null){
-                return csd.getProviderDirectory().getProviders();
-            }*/
+            CSD csd=csdQueryClient.callStandardStoredFunction(requestParams, StoredQueryConstants.PROVIDER_SEARCH, httpAddress, null);
+            return csd.getProviderDirectory().getProviders();
         } catch (Exception ex) {
             ex.printStackTrace(System.out);
             throw new RuntimeException(ex);
         }
-        
-        return null;
-        
+
     }
-    
-      public static List<Facility> getFacilities(RequestParams requestParams, String httpAddress) {
-        CareServicesRequest careServicesRequest = new CareServicesRequest(new Function(StoredQueryConstants.FACILITY_SEARCH, requestParams));
-        String xmlRequestBody = careServicesRequest.marshal();
-        System.out.println("xmlBody=" + xmlRequestBody);
-        String xmlResponseBody = JaxCsdUtil.executeXmlPost(xmlRequestBody, httpAddress);
-        //System.out.print("response=" + xmlResponseBody);
+
+    public static List<Facility> getFacilities(RequestParams requestParams, String httpAddress) {
+         CsdQueryClient csdQueryClient = new CsdQueryClient();
         try {
-            /*CSD csd = CSDHandler.getCSD(xmlResponseBody);
-            if(csd!=null && csd.getProviderDirectory()!=null){
-                return csd.getFacilityDirectory().getFacilities();
-            }*/
+            CSD csd=csdQueryClient.callStandardStoredFunction(requestParams, StoredQueryConstants.FACILITY_SEARCH, httpAddress, null);
+            return csd.getFacilityDirectory().getFacilities();
         } catch (Exception ex) {
             ex.printStackTrace(System.out);
         }
-        
+
         return null;
-        
+
     }
-      
-      public static String pushCSD(RequestParams requestParams, String httpAddress) {
+
+    public static String pushCSD(RequestParams requestParams, String httpAddress) {
         CareServicesRequest careServicesRequest = new CareServicesRequest(new Function(StoredQueryConstants.PUSH_REQUEST, requestParams));
         String xmlRequestBody = careServicesRequest.marshal();
-        System.out.println("xmlBody=" + xmlRequestBody);
         String xmlResponseBody = JaxCsdUtil.executeXmlPost(xmlRequestBody, httpAddress);
         return xmlResponseBody;
-        
+
     }
-      
-     
-      
-    
-    
-    
-    
-    
+
     public static void main(String[] args) {
         RequestParams requestParams = new RequestParams();
         //requestParams.setUniqueID(new Provider("2.25.231042483471474750523306613509563984609"));
         //equestParams.setCommonName("chigo");
         //requestParams.setStart(20000);
-        requestParams.setOtherID(new OtherID("08-563960B29", "mohaffairs.org.zw"));
+        //requestParams.setOtherID(new OtherID("08-563960B29", "mohaffairs.org.zw"));
         requestParams.setMax(5);
-        List<Provider> providers=getProviders(requestParams, "http://192.168.1.23:8984/CSD/csr/ncz/careServicesRequest");
-        if(providers!=null && !providers.isEmpty()){
-            for(Provider provider: providers){
-                System.out.println("Surname :"+provider.getTopName().getSurname());
+        List<Provider> providers = getProviders(requestParams, "http://192.168.1.23:8984/CSD/csr/ncz/careServicesRequest");
+        if (providers != null && !providers.isEmpty()) {
+            for (Provider provider : providers) {
+                System.out.println("Surname :" + provider.getTopName().getSurname());
             }
         }
     }
